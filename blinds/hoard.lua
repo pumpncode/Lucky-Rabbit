baf8c7d8758474f4e11745d485c60fd5aeb6b215
Xavier Torrescano <xmtrocks@gmail.com>
2025-04-07 15:28:40 -0700

    set hoard's minimum ante to 5

--- blinds/hoard.lua
+++ blinds/hoard.lua
@@ -3,7 +3,7 @@ SMODS.Blind {
     atlas = "Blinds",
     pos = { x = 0, y = 0 },
     discovered = true,
-    boss = { min = 1 },
+    boss = { min = 5 },
     boss_colour = HEX('963f39'),
     stay_flipped = function(self, area, card)
         if not card:is_face() and area == G.hand then
