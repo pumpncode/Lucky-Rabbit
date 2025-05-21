e2bd1e7670d87898d13bfd622a32555e672149a1
english5040 <ejwu@andrew.cmu.edu>
2025-05-21 07:48:05 -0400

    Harmony deck reimplementation

--- content/decks/harmony.lua
+++ content/decks/harmony.lua
@@ -8,17 +8,6 @@ SMODS.Back {
         discards = -2
     },
     apply = function(self, back)
-        G.E_MANAGER:add_event(Event({
-            trigger = 'after',
-            delay = 0.3,
-            func = function()
-                local rank = pseudorandom_element(SMODS.Ranks, pseudoseed('harmony')) or 'Ace'
-                local suit = pseudorandom_element(SMODS.Suits, pseudoseed('harmony')) or 'Spades'
-                for i = 1, #G.playing_cards do
-                    SMODS.change_base(G.playing_cards[i], suit.key, rank.key)
-                end
-                return true
-            end
-        }))
+        G.GAME.starting_params.fmod_harmony = true
     end
 }
