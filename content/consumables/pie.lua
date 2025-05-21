488cdea01ffc681c90063590f250e610ecacf724
Xavier Torrescano <xmtrocks@gmail.com>
2025-05-20 18:04:36 -0700

    make consumables that unhighlight clear hand text

--- content/consumables/pie.lua
+++ content/consumables/pie.lua
@@ -40,7 +40,7 @@ SMODS.Consumable {
             local percent = 0.85 + (i-0.999)/(#G.hand.highlighted-0.998)*0.3
             G.E_MANAGER:add_event(Event({trigger = 'after',delay = 0.15,func = function() G.hand.highlighted[i]:flip();play_sound('tarot2', percent, 0.6);G.hand.highlighted[i]:juice_up(0.3, 0.3);return true end }))
         end
-        G.E_MANAGER:add_event(Event({trigger = 'after', delay = 0.2,func = function() G.hand:unhighlight_all(); return true end }))
+        G.E_MANAGER:add_event(Event({trigger = 'after', delay = 0.2,func = function() update_hand_text({immediate = true, nopulse = true, delay = 0}, {mult = 0, chips = 0, level = '', handname = ''}); G.hand:unhighlight_all(); return true end }))
         delay(0.5)
     end,
     can_use = function(self, card)
