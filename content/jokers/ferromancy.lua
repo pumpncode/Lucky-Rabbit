4204b687dc0042788032ff5e50b9dbd2ebe8fc59
BepisFever <nguyendoangiabao220808@gmail.com>
2025-05-25 13:30:01 +0700

    Update ferromancy.lua

--- content/jokers/ferromancy.lua
+++ content/jokers/ferromancy.lua
@@ -1,23 +1,26 @@
 SMODS.Enhancement:take_ownership('m_steel',
     {
-        loc_vars = function(self, info_queue, card)
-            if #SMODS.find_card('j_fmod_ferromancy') > 0 then
-                return {
-                    vars = { '$' .. (card.ability.h_dollars or 5) },
-                    key = 'm_gold'
-                }
-            else
-                return {
-                    vars = { card.ability.h_x_mult },
-                }
-            end
-        end,
         update = function(self, card, dt)
             if #SMODS.find_card('j_fmod_ferromancy') > 0 then
                 card.ability.h_x_mult = 0
                 card.ability.h_dollars = 5
             else
                 card.ability.h_x_mult = 1.5
+            end
+        end
+    },
+    true
+)
+
+SMODS.Enhancement:take_ownership('m_gold',
+    {
+        update = function(self, card, dt)
+            if #SMODS.find_card('j_fmod_ferromancy') > 0 then
+                card.ability.h_x_mult = 2
+                card.ability.h_dollars = 0
+            else
+                card.ability.h_x_mult = 1
+                card.ability.h_dollars = 3
             end
         end
     },
