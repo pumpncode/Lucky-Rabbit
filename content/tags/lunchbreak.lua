eaa2d9ff00fdec371c4be26afebd4e1a52783538
english5040 <ejwu@andrew.cmu.edu>
2025-05-27 03:16:00 -0400

    Lunchbreak is immediate

--- content/tags/lunchbreak.lua
+++ content/tags/lunchbreak.lua
@@ -3,18 +3,21 @@ SMODS.Tag {
     atlas = "Tags",
     pos = { x = 1, y = 0 },
     config = {
+        type = "immediate",
     },
     discovered = true,
     loc_vars = function(self, info_queue, card)
         info_queue[#info_queue+1] = G.P_CENTERS.e_negative
     end,
     apply = function(self, tag, context)
-        tag:yep('+', G.C.GREEN, function()
-            local key = LR_UTIL.get_food_jokers('lunchbreak')
-            SMODS.add_card({set = 'Joker', key = key, edition = 'e_negative'})
+        if context.type == self.config.type then
+            tag:yep('+', G.C.GREEN, function()
+                local key = LR_UTIL.get_food_jokers('lunchbreak')
+                SMODS.add_card({set = 'Joker', key = key, edition = 'e_negative'})
+                return true
+            end)
+            tag.triggered = true
             return true
-        end)
-        tag.triggered = true
-        return true
+        end
     end
 }
