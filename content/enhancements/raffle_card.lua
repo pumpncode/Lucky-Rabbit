1de2320be36b51fc251c9e40f5fbc9125809fced
Xavier Torrescano <xmtrocks@gmail.com>
2025-05-28 21:43:13 -0700

    fix raffle cards putting jimbo in the consumable area (hopefully)

--- content/enhancements/raffle_card.lua
+++ content/enhancements/raffle_card.lua
@@ -14,14 +14,29 @@ SMODS.Enhancement {
         if context.main_scoring and context.cardarea == G.play then
             if pseudorandom("raffle_cons") < G.GAME.probabilities.normal / card.ability.extra.chance and (#G.consumeables.cards + G.GAME.consumeable_buffer < G.consumeables.config.card_limit) then
                 G.GAME.consumeable_buffer = G.GAME.consumeable_buffer + 1
-                local poss = {}
-                for _, k in ipairs(SMODS.ConsumableType.ctype_buffer) do
-                    poss[#poss+1] = k
+                local selected = ""
+                local passed = false
+                local tries = 50
+                while tries > 0 and not passed do -- modified from a cryptid function, could easily be absolute garbage
+                    tries = tries - 1
+                    passed = false
+                    local key = pseudorandom_element(G.P_CENTER_POOLS.Consumeables, pseudoseed("raffle_cons")).key
+                    selected = G.P_CENTERS[key]
+                    if not (selected["hidden"] or (G.GAME and G.GAME["hidden"] and G.GAME["hidden"][selected]) or false) then
+                        passed = true
+                    end
+                    if passed or tries <= 0 then
+                        if tries <= 0 then
+                            selected = "c_strength"
+                        else
+                            selected = selected
+                        end
+                    end
                 end
                 G.E_MANAGER:add_event(Event({
                     delay = 0.3,
                     func = function()
-                        SMODS.add_card( { set = pseudorandom_element(poss, pseudoseed("raffle_cons")), area = G.consumables, soulable = false })
+                        SMODS.add_card( { area = G.consumeables, soulable = false, key = selected.key })
                         G.GAME.consumeable_buffer = 0
                         return true
                     end
